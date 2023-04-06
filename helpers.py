8c5df2d16a2425cff9b2d8c647a2bdab4e63040e
Rani Woerner <rwoerner@umich.edu>
2023-04-06 12:26:12 -0400

    filtered down events

--- helpers.py
+++ helpers.py
@@ -1,4 +1,17 @@
 def check_for_freebies(description):
-    if 'free' in str(description):
-        return True
+
+    description_lst = description.split()
+    punct = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    for i in range(0, len(description_lst)):
+        tmp = description_lst[i]
+        tmp = tmp.lower()
+        for letter in tmp:
+            if letter in punct:
+                tmp = tmp.replace(letter, "")
+        if (tmp == 'free'):
+            if (i < (len(description_lst) - 1)):
+                if (description_lst[i+1].lower() != 'to'):
+                    return True
+
     return False

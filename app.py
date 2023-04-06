7b4c55b5582e727f41a0ea059a018af8264d0eb1
tayheath <tayheath@umich.edu>
2023-04-06 14:14:37 -0400

    calendar and css update

--- app.py
+++ app.py
@@ -9,10 +9,18 @@ app = Flask(__name__, template_folder='templates/', static_folder='static/')
 
 @app.route('/')
 def home():
-    events = []
-    freebies = []
+    events = {}
+    freebies = {}
     for day in (date.today() + timedelta(n) for n in range(7)):
-        events.extend(get_events_happening('https://events.umich.edu/day/' + str(day)))
-    freebies.extend(filter_events_happening(events))
+        if day in events:
+            events[day].extend(get_events_happening('https://events.umich.edu/day/' + str(day)))
+        else:
+            events[day] = [get_events_happening('https://events.umich.edu/day/' + str(day))]
+    for day in events:
+        dayo = day.strftime("%A %B %d")
+        if dayo in freebies:
+            freebies[dayo].extend(filter_events_happening(events[day][0]))
+        else:
+            freebies[dayo] = [filter_events_happening(events[day][0])]
 
     return render_template('index.html', freebies=freebies)

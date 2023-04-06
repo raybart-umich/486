21768169b7e2ddf357af99f765c589a30d33e059
Martin Vega-Velasco <martinvv@umich.edu>
2023-04-06 19:32:22 -0400

    minor performance improvements and css fixes

--- happening.py
+++ happening.py
@@ -1,16 +1,21 @@
-from urllib.request import Request, urlopen
+import requests
 from bs4 import BeautifulSoup
 from helpers import check_for_freebies
 from unidecode import unidecode
+
+def get_soup(url):
+    s = requests.Session()
+    s.headers.update({'User-Agent': 'Mozilla/5.0'})
+    req = s.get(url=url)
+    page = req.text
+    return BeautifulSoup(page, 'html.parser')
 
 def get_events_happening(url):
     """Retrieve events from URL."""
     events = []
     base = 'https://events.umich.edu'
 
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    page = urlopen(req).read()
-    soup = BeautifulSoup(page, 'html.parser')
+    soup = get_soup(url)
     results = soup.findAll(True, {'class':['event-listing', 'event-listing event-single']})
 
     for result in results:
@@ -24,9 +29,7 @@ def get_events_happening(url):
 def filter_events_happening(events):
     freebie_events = []
     for url in events:
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        page = urlopen(req).read()
-        soup = BeautifulSoup(page, 'html.parser')
+        soup = get_soup(url)
         try:
             description = soup.find(class_='event-description-text').text
             if check_for_freebies(description):

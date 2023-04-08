245ad01da4c9fd19fb955d3436cbbc25a26b7a70
Rani Woerner <rwoerner@umich.edu>
2023-04-08 16:56:15 -0400

    seeing if next word is a noun

--- helpers.py
+++ helpers.py
@@ -1,3 +1,7 @@
+import nltk
+
+nltk.download('averaged_perceptron_tagger')
+
 def check_for_freebies(description):
 
     description_lst = description.split()
@@ -11,7 +15,15 @@ def check_for_freebies(description):
                 tmp = tmp.replace(letter, "")
         if (tmp == 'free'):
             if (i < (len(description_lst) - 1)):
-                if (description_lst[i+1].lower() != 'to'):
+                # clean next word
+                next_word = description_lst[i+1]
+                next_word = next_word.lower()
+                for letter in next_word:
+                    if letter in punct:
+                        next_word = next_word.replace(letter, "")
+                tuple_tag = nltk.pos_tag(next_word)
+                tag = tuple_tag[0][1]
+                if(tag == 'NN' or tag == 'NNS' or tag == 'NNPS' or tag == 'NNP'):
                     return True
 
     return False

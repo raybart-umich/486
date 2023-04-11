8ba7eedbdd1dd283322a2817b879b935d9f0cb30
Martin Vega-Velasco <martinvv@umich.edu>
2023-04-10 23:54:11 -0400

    updated README and changed filepath to work on any computer

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__, template_folder='templates/', static_folder='static/')
 @app.route('/')
 def home():
     freebies = {}
-    with open("/home/martinvv/FreebieFinder/events.txt", "r") as f:
+    with open("events.json", "r") as f:
         freebies = json.loads(f.read())
     return render_template('index.html', freebies=freebies)
